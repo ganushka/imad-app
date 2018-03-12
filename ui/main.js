325fd84baf6e621cff2732b98e5d2be389a108ce
ganushka <36573230+ganushka@users.noreply.github.com>
2018-03-13 01:44:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+10;
+    img.style.marginLeft=marginLeft+"px";
+}
+img.onclick=funtion()
+{
+    var interval=setInterval(moveright, 50);
+}
