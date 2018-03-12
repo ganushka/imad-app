2044d46f2f356b27022271a8d0326e8fa4222abb
ganushka <36573230+ganushka@users.noreply.github.com>
2018-03-13 01:50:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var marginleft=0;
 function moveright()
 {
     marginleft=marginleft+10;
-    img.style.marginLeft=margileft+"px";
+    img.style.marginLeft=marginleft+"px";
 }
 
 img.onclick =function(){
